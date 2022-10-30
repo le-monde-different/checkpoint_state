a40d0b215b2f5129a32620881c221a19a9a51ab3
le-monde-different <fiattipo197@gmail.com>
2022-10-30 22:28:02 +0100

    3em

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import Imag  from './images/img1.png';
 import  "./App.css" 
-import { Button } from 'bootstrap';
+
 
 class App extends Component{
   constructor(){
@@ -13,9 +13,11 @@ class App extends Component{
                 fullName : "RHIF MAROUEN",
                 bio : "DEVLOPPEUR WEB", 
                 imgSrc: Imag, 
-                profession:"DOVLOPPEUR "
-              },
-    }
+                profession:"DOVLOPPEUR ",
+                },
+                time:0,
+              }
+    
     this.clickHandler = this.clickHandler.bind(this);
   }
   
@@ -28,6 +30,14 @@ class App extends Component{
       }
     });
   }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((previousState) => ({
+      time: previousState.time + 1,
+      }));
+    }, 1000);
+  }
+
 
   render(){
     if(this.state.show)
@@ -35,10 +45,11 @@ class App extends Component{
       <div className="container">
          <button type="button" className="btn btn-warning" onClick={this.clickHandler}>Hide</button>
         <br/>
-        <img src={Imag} alt="imag" width="400" height="430" />
+        <img src={Imag} alt="imag" width="300" height="230" />
         <h1>{this.state.Person.fullName}</h1>
         <h2>{this.state.Person.bio}</h2>
         <h2>{this.state.Person.profession}</h2>
+        <h2>{this.state.time}</h2>
       </div>
       );
       else return (
